b5648cb42ad3abd8c051d6f3fa4622cefabcf5ff
sidarthkrishna <sidarth.krishna@gmail.com>
2025-03-29 23:53:25 -0400

    fixing error

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import { useEffect, useState, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
